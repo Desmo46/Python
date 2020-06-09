c5fd805acaad371f89eb6c6812166b164f95f049
pc <p.j.clements@me.com>
2020-06-10 01:29:34 +0200

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -6,4 +6,12 @@ print (x % 5)
 
 print(len("Hello g"))
 
-import pandas
+import pandas as pd
+
+cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
+        'Price': [22000,25000,27000,35000]
+        }
+
+df = pd.DataFrame(cars, columns = ['Brand', 'Price'])
+
+print (df[0:1])
